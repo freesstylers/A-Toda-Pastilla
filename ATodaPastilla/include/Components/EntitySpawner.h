0c8a71b2fafeaed2923b086ff01628f72fd62d48
BraisMorgade <braismor@ucm.es>
2020-04-21 20:47:21 +0200

    Empezado Spawner de Entidades

---
+++ ATodaPastilla/include/Components/EntitySpawner.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Entity\Component.h"
+class EntitySpawner :public Component
+{
+public:
+	EntitySpawner(json& args);
+	~EntitySpawner() {};
+	void init(json& j) override;
+	void update() override;
+	Entity* spawnEntity(std::string prefab);
+
+private:
+	std::string defaultPrefab;
+	std::list<Entity*>items;
+};
+
