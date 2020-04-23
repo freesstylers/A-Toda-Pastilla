547db40391ff5850f398ea482fd8c60cd1a440c4
BraisMorgade <braismor@ucm.es>
2020-04-23 23:20:58 +0200

    BorrandoPastillas

--- ATodaPastilla/src/Components/ProjectileBehaviour.cpp
+++ ATodaPastilla/src/Components/ProjectileBehaviour.cpp
@@ -7,6 +7,10 @@ ProjectileBehaviour::ProjectileBehaviour(json& args):Component(args)
 {
 	speed = 0;
 	dir = Vector3(0, 0, 0);
+	bordeSup = 500;
+	bordeInf = -500;
+	bordeDer = 500;
+	bordeIzq = -500;
 }
 
 void ProjectileBehaviour::init(json& args)
@@ -17,6 +21,10 @@ void ProjectileBehaviour::init(json& args)
 void ProjectileBehaviour::update()
 {
 	Transform* t = getEntity()->getComponent<Transform>("Transform");
+	if (t->getPosition().X > bordeDer || t->getPosition().X < bordeIzq ||
+		t->getPosition().Z > bordeSup || t->getPosition().Z < bordeInf) {
+		getEntity()->setActive(false);
+	}
 	Vector3 pos = t->getPosition();
 	float deltatime = MotorCasaPaco::getInstance()->DeltaTime() / 1000.0f;
 	pos += dir.Normalized(dir) * speed * deltatime;
