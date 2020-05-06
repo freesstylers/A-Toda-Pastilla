35763e393e8c20e6058e0b9772d004e3d4bc69ca
IgnOry <iory@ucm.es>
2020-05-06 13:27:28 +0200

    pantalla completa stuff
    y cosas de cegui

--- ATodaPastilla/ATodaPastilla/ATodaPastilla.cpp
+++ ATodaPastilla/ATodaPastilla/ATodaPastilla.cpp
@@ -303,7 +303,6 @@ WinMain(HINSTANCE hinstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdSh
 		return 0;
 	}
 
-	MotorCasaPaco::getInstance()->getGUI_Manager()->getInstance()->setupDefaultResources();
 	MotorCasaPaco::getInstance()->getGUI_Manager()->loadScheme("A_Toda_Pastilla.scheme");
 	MotorCasaPaco::getInstance()->getGUI_Manager()->setMouseCursor("A_Toda_Pastilla/Mouse_Arrow");
 
